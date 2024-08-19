0f8776509cd27bfd78623300a570a94af4b4235e
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-08-20 00:13:28 +0100

    Add method to create array of specific length

---
+++ lib/array.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+const DynamicArray = struct {
+    // Length of the dynamic array as seen by the user
+    len: usize,
+    // Slice of internal static array used to provide the dynamic array
+    slice: []u8,
+
+    pub fn new(allocator: std.mem.Allocator, len: usize) std.mem.Allocator.Error!DynamicArray {
+        const slice = try allocator.alloc(u8, len);
+        return DynamicArray{
+            .len = len,
+            .slice = slice,
+        };
+    }
+};
+
+test "create dynamic array with given length" {
+    const len = 5;
+    const allocator = std.testing.allocator;
+    const arr = try DynamicArray.new(allocator, len);
+    try std.testing.expect(arr.len == len);
+}
