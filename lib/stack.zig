2a9d68c4ec7efdf5b4e1199f1afc8d2e2a465bf5
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-09-12 23:08:13 +0100

    Return empty error in stack peek method

--- lib/stack.zig
+++ lib/stack.zig
@@ -1,14 +1,36 @@
 const std = @import("std");
 
+const list = @import("linked_list.zig");
+
 const Stack = struct {
-    size: usize,
+    const Error = error{
+        EmptyStack,
+    };
+
+    list: list.SinglyLinkedList,
 
     fn new() Stack {
-        return Stack{ .size = 0 };
+        return Stack{ .list = list.SinglyLinkedList.new() };
+    }
+
+    fn size(self: Stack) usize {
+        return self.list.len;
+    }
+
+    fn peek(self: Stack) Error!void {
+        if (self.size() == 0) {
+            return Error.EmptyStack;
+        }
     }
 };
 
 test "create stack" {
     const stack = Stack.new();
-    try std.testing.expectEqual(0, stack.size);
+    try std.testing.expectEqual(0, stack.size());
+}
+
+test "return error if peeking on empty stack" {
+    const stack = Stack.new();
+    const ret = stack.peek();
+    try std.testing.expectError(Stack.Error.EmptyStack, ret);
 }
