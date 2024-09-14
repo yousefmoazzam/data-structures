0310fff7e96dd6bf9dfe335e3bcee6c955ffc380
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-09-14 18:11:58 +0100

    Add enqueue method to queue

--- lib/queue.zig
+++ lib/queue.zig
@@ -1,30 +1,63 @@
 const std = @import("std");
 
+const linked_list = @import("linked_list.zig");
+
 const Queue = struct {
-    size: usize,
+    list: linked_list.SinglyLinkedList,
 
     const Error = error{
         EmptyQueue,
     };
 
     fn new() Queue {
-        return Queue{ .size = 0 };
+        return Queue{ .list = linked_list.SinglyLinkedList.new() };
     }
 
-    fn peek(self: Queue) Error!void {
-        if (self.size == 0) {
+    fn peek(self: Queue) Error!u8 {
+        if (self.list.len == 0) {
             return Error.EmptyQueue;
         }
+
+        if (self.list.get(0)) |value| {
+            return value;
+        } else |_| {
+            // The only error in the error set that `SinglyLinkedList.get()` can return is
+            // `OutOfBounds`. Furthermore, because `peek()` is only ever getting index 0, the
+            // only way an error can be returned from calling `SinglyLinkedList.get()` is if
+            // the linked list is empty. However, this has already been handled at the start of
+            // the method, returning an `EmptyQueue` error.
+            //
+            // Therefore, an error should never be returned at this point, so this else branch
+            // should be considered unreachable.
+            unreachable;
+        }
+    }
+
+    fn size(self: Queue) usize {
+        return self.list.len;
+    }
+
+    fn enqueue(self: *Queue, allocator: std.mem.Allocator, value: u8) std.mem.Allocator.Error!void {
+        try self.list.append(allocator, value);
     }
 };
 
 test "create queue" {
     const queue = Queue.new();
-    try std.testing.expectEqual(0, queue.size);
+    try std.testing.expectEqual(0, queue.size());
 }
 
 test "return error if peeking on empty queue" {
     const queue = Queue.new();
     const ret = queue.peek();
     try std.testing.expectError(Queue.Error.EmptyQueue, ret);
+}
+
+test "enqueue one element to queue" {
+    var queue = Queue.new();
+    const allocator = std.testing.allocator;
+    const value = 3;
+    try queue.enqueue(allocator, value);
+    try std.testing.expectEqual(1, queue.size());
+    try std.testing.expectEqual(value, try queue.peek());
 }
