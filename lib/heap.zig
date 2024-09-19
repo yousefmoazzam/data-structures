0e0db35c2d2a0c592667f640e20831d8e64e16f7
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-09-19 22:20:27 +0100

    Return empty error in binary heap peek method

--- lib/heap.zig
+++ lib/heap.zig
@@ -1,6 +1,10 @@
 const std = @import("std");
 
 const BinaryHeap = struct {
+    const Error = error{
+        EmptyHeap,
+    };
+
     size: usize,
 
     pub fn new() BinaryHeap {
@@ -10,9 +14,21 @@ const BinaryHeap = struct {
     pub fn isEmpty(self: BinaryHeap) bool {
         return self.size == 0;
     }
+
+    pub fn peek(self: BinaryHeap) Error!void {
+        if (self.isEmpty()) {
+            return Error.EmptyHeap;
+        }
+    }
 };
 
 test "create binary heap" {
     const heap = BinaryHeap.new();
     try std.testing.expectEqual(true, heap.isEmpty());
+}
+
+test "return error if peeking on empty heap" {
+    const heap = BinaryHeap.new();
+    const ret = heap.peek();
+    try std.testing.expectError(BinaryHeap.Error.EmptyHeap, ret);
 }
