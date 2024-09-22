75bbccf0c4cd9adbdfaf4e4d030ca053aef8be4b
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-09-22 17:37:37 +0100

    Enable dequeueing multiple elements from multi-element binary heap

--- lib/heap.zig
+++ lib/heap.zig
@@ -132,9 +132,96 @@ const BinaryHeap = struct {
             }
         }
 
-        // TODO: Swap value with root value, remove + return, then bubble-down new root value.
-        // For now, to satisfy the return type, return any `u8` value
-        return 0;
+        // If execution has reached here, then `self.arr.len > 1`, so getting the 0th index
+        // element using `DynamicArray.get(0)` should never return an `OutOfBounds` error.
+        // Hence, unreachable.
+        const value_to_dequeue = if (self.arr.get(0)) |val| val else |_| unreachable;
+        // `self.arr.len - 1` should be a valid index in any array with length > 1, so
+        // `DynamicArray.get(self.arr.len - 1)` should never return an `OutOfBounds` error.
+        // Hence, unreachable.
+        const value_to_swap = if (self.arr.get(self.arr.len - 1)) |val| val else |_| unreachable;
+
+        // Replace root value with end value (no need to replace end value with value to
+        // dequeue, because it's going to be removed anyway, and we already have its value
+        // stored in `value_to_dequeue`)
+        //
+        // The reasoning for the use of unreachable is the same as for both cases above
+        // regarding `DynamicArray.get()`, but now for  `DynamicArray.set()`
+        if (self.arr.set(0, value_to_swap)) |_| {} else |_| unreachable;
+
+        // Remove end value
+        if (self.arr.delete(self.arr.len - 1)) |_| {} else |_| unreachable;
+
+        // Bubble-down new root value
+        self.bubble_down(value_to_swap);
+
+        // Return the value that was removed
+        return value_to_dequeue;
+    }
+
+    fn bubble_down(self: *BinaryHeap, newRoot: u8) void {
+        var hasFinishedBubbling = false;
+        var idx: usize = 0;
+
+        while (!hasFinishedBubbling) {
+            const leftChildIdx = 2 * idx + 1;
+            const rightChildIdx = 2 * idx + 2;
+
+            // If the left child index is past the end of the array, then so will the right
+            // child index too. In this case, bubbling-down has completed and iteration should
+            // terminate.
+            if (leftChildIdx > self.arr.len - 1) {
+                hasFinishedBubbling = true;
+                continue;
+            }
+
+            // If execution has gotten this far, then the left child must be within the bounds
+            // of the array.
+            if (rightChildIdx > self.arr.len - 1) {
+                // If only the right child index is past the end of the array but the left
+                // child is within the array, then only the left child exists and thus must be
+                // the one to compare with in the bubbling-down process.
+                const childValue = if (self.arr.get(leftChildIdx)) |value| value else |_| unreachable;
+                if (newRoot > childValue) {
+                    // Swap value and child value
+                    //
+                    // If the left child index is within the bounds of the array, then so must
+                    // the parent of it. This means that an `OutOfBounds` error for the use of
+                    // `DynamicArray.set`()` isn't possible. Hence, unreachable.
+                    if (self.arr.set(idx, childValue)) |_| {} else |_| unreachable;
+                    if (self.arr.set(leftChildIdx, newRoot)) |_| {} else |_| unreachable;
+                }
+
+                // Because there was only one child element in the swap above, bubbling-down
+                // has gone as far as it can, so terminate the process.
+                hasFinishedBubbling = true;
+            } else {
+                // If execution has gotten to this branch, then both the left and right
+                // children exist.
+
+                // Both children exist, so `DynamicArray.get()` should not return an
+                // `OutOfBounds` error. Hence, unreachable.
+                const leftChildValue = if (self.arr.get(leftChildIdx)) |val| val else |_| unreachable;
+                const rightChildValue = if (self.arr.get(rightChildIdx)) |val| val else |_| unreachable;
+
+                // Find which child element is smaller
+                const smallerChildIdx = if (leftChildValue < rightChildValue) leftChildIdx else rightChildIdx;
+                const smallerChildValue = if (smallerChildIdx == leftChildIdx) leftChildValue else rightChildValue;
+                if (newRoot > smallerChildValue) {
+                    // Swap value and child value
+                    //
+                    // Both the smaller child index and the index of the bubbled-down value
+                    // must be within the bounds of the array, so `OutOfBounds` shouldn't be
+                    // returned by `DynamicArray.set()` in either case. Hence, unreachable.
+                    if (self.arr.set(idx, smallerChildValue)) |_| {} else |_| unreachable;
+                    if (self.arr.set(smallerChildIdx, newRoot)) |_| {} else |_| unreachable;
+                }
+
+                // Carry onto the next iteration to check the new children indices and see if
+                // bubbling-down should carry on or not.
+                idx = smallerChildIdx;
+            }
+        }
     }
 };
 
@@ -207,6 +294,30 @@ test "dequeue only element from single element heap" {
     // empty
     try std.testing.expectEqual(value, try heap.dequeue());
     try std.testing.expectEqual(true, heap.isEmpty());
+
+    // Free heap
+    try heap.free(allocator);
+}
+
+test "dequeue multiple elements from binary heap" {
+    const allocator = std.testing.allocator;
+    var heap = try BinaryHeap.new(allocator);
+    const values = [_]u8{ 6, 3, 7, 2, 9, 8, 1 };
+    const expectedPriorityOrdering = [_]u8{ 1, 2, 3, 6, 7, 8, 9 };
+
+    // Sanity check that the test is correct: make sure `values` and `expectedHighestPriority`
+    // are the same length
+    try std.testing.expectEqual(values.len, expectedPriorityOrdering.len);
+
+    // Enqueue elements onto heap
+    for (values) |value| {
+        try heap.enqueue(allocator, value);
+    }
+
+    // Dequeue all elements, and check the ordering of elements follows the expected priority
+    for (0..values.len) |i| {
+        try std.testing.expectEqual(expectedPriorityOrdering[i], try heap.dequeue());
+    }
 
     // Free heap
     try heap.free(allocator);
