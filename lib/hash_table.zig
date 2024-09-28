ab476c2ba9ba389dd8d3ccd0e75788dfabc5c6b2
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-09-28 17:56:47 +0100

    Add method to create hash table instance

--- lib/hash_table.zig
+++ lib/hash_table.zig
@@ -4,12 +4,28 @@ const std = @import("std");
 /// of type `T`
 pub fn HashTable(comptime T: type) type {
     return struct {
+        // Public
+        size: usize,
+
+        // Private / internal
+        slice: []T,
+
         pub fn KeyType() type {
             return u8;
         }
 
         pub fn ValueType() type {
             return T;
+        }
+
+        const Self = @This();
+
+        pub fn new(allocator: std.mem.Allocator) std.mem.Allocator.Error!Self {
+            const vals = try allocator.alloc(T, 0);
+            return Self{
+                .size = 0,
+                .slice = vals,
+            };
         }
     };
 }
@@ -20,4 +36,10 @@ test "create hash table type mapping u8 keys to u16 values" {
     const hash_table_type = HashTable(value_type);
     try std.testing.expectEqual(key_type, hash_table_type.KeyType());
     try std.testing.expectEqual(value_type, hash_table_type.ValueType());
+}
+
+test "create empty hash table" {
+    const allocator = std.testing.allocator;
+    const hash_table = try HashTable(u8).new(allocator);
+    try std.testing.expectEqual(0, hash_table.size);
 }
