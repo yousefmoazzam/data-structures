014e13fd0b30b9d7dc85d59f0783d93454a92a81
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-10-11 22:04:16 +0100

    Implement proper binary search tree element insertion

--- lib/binary_search_tree.zig
+++ lib/binary_search_tree.zig
@@ -66,28 +66,91 @@ pub const InorderTraversalEagerIterator = struct {
 pub const BinarySearchTree = struct {
     allocator: std.mem.Allocator,
     slice: []?u8,
-    count: usize,
+    layers: usize,
 
     pub fn new(allocator: std.mem.Allocator) std.mem.Allocator.Error!BinarySearchTree {
-        return BinarySearchTree{ .slice = try allocator.alloc(?u8, 0), .allocator = allocator, .count = 0 };
+        return BinarySearchTree{ .slice = try allocator.alloc(?u8, 0), .allocator = allocator, .layers = 0 };
     }
 
-    pub fn insert(self: *BinarySearchTree, value: ?u8) std.mem.Allocator.Error!void {
-        // TODO: Naive insertion implementation, purely to be able to get elements into the
-        // slice for the purposes of testing inorder traversal algorithm.
-        //
-        // Properly implement this later.
-        if (self.slice.len == 0) {
-            const slice = try self.allocator.alloc(?u8, 15);
-            for (slice) |*element| {
-                element.* = null;
-            }
-            self.allocator.free(self.slice);
-            self.slice = slice;
+    fn calculateNumberOfElementsInNextLayer(self: BinarySearchTree) usize {
+        return self.slice.len + std.math.pow(usize, 2, self.layers);
+    }
+
+    fn addLayer(self: *BinarySearchTree) std.mem.Allocator.Error!void {
+        const new_slice = try self.allocator.alloc(?u8, self.calculateNumberOfElementsInNextLayer());
+        for (0..self.slice.len) |i| {
+            new_slice[i] = self.slice[i];
         }
 
-        self.slice[self.count] = value;
-        self.count += 1;
+        // Set `null` values for the nodes in the newly added layer
+        for (self.slice.len..new_slice.len) |i| {
+            new_slice[i] = null;
+        }
+
+        self.allocator.free(self.slice);
+        self.slice = new_slice;
+        self.layers += 1;
+    }
+
+    pub fn insert(self: *BinarySearchTree, value: u8) std.mem.Allocator.Error!void {
+        if (self.slice.len == 0) {
+            const slice = try self.allocator.alloc(?u8, 1);
+            slice[0] = value;
+            self.allocator.free(self.slice);
+            self.slice = slice;
+            self.layers += 1;
+            return;
+        }
+
+        // If execution has reached here, then the BST is non-empty, and now must recurse down
+        // the tree until the appropriate place to insert the given value has been found
+        try self.insert_recurse(0, value);
+    }
+
+    fn insert_recurse(self: *BinarySearchTree, idx: usize, value: u8) std.mem.Allocator.Error!void {
+        const current_value = if (self.slice[idx]) |val| val else {
+            self.slice[idx] = value;
+            return;
+        };
+
+        const is_value_less_than_current = value < current_value;
+        if (is_value_less_than_current) {
+            const left_child_idx: usize = 2 * idx + 1;
+
+            if (left_child_idx < self.slice.len) {
+                // Carry on recursing down left subtree
+                return try self.insert_recurse(left_child_idx, value);
+            }
+
+            // Value is smaller than current value, but the index of the left child of the
+            // current node isn't within bounds of the underlying array, so cannot recurse
+            // further. Need to add another layer to the BST, and then insert value as the left
+            // child of the current node.
+            if (left_child_idx >= self.slice.len) {
+                try self.addLayer();
+            }
+            self.slice[left_child_idx] = value;
+            return;
+        }
+
+        // If execution has reached here, then the value is >= to the current value.
+        //
+        // NOTE: Not handling duplicate values for now, so assume value is > current value, so
+        // need to recurse down right subtree.
+        const right_child_idx: usize = 2 * idx + 2;
+        if (right_child_idx < self.slice.len) {
+            // Carry on recursing down right subtree
+            return try self.insert_recurse(right_child_idx, value);
+        }
+
+        // If execution has reached here, then the right child index isn;t within bounds, so
+        // cannot recurse further. Need to add another layer to the BST, and then insert value
+        // as the right child of the current node.
+        if (right_child_idx >= self.slice.len) {
+            try self.addLayer();
+        }
+        self.slice[right_child_idx] = value;
+        return;
     }
 
     pub fn inorderTraversal(self: BinarySearchTree) std.mem.Allocator.Error!InorderTraversalEagerIterator {
@@ -120,7 +183,7 @@ test "inorder traversal iterator produces correct ordering of visited nodes" {
     const allocator = std.testing.allocator;
     var bst = try BinarySearchTree.new(allocator);
     const values = [_]u8{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
-    const expected_ordering = [_]u8{ 7, 3, 8, 1, 9, 4, 10, 0, 11, 5, 12, 2, 13, 6, 14 };
+    const expected_ordering = values;
 
     // Insert values into BST
     for (values) |value| {
@@ -146,8 +209,8 @@ test "inorder traversal iterator produces correct ordering of visited nodes" {
 test "inorder traversal over BST with null nodes produces correct ordering of visited nodes" {
     const allocator = std.testing.allocator;
     var bst = try BinarySearchTree.new(allocator);
-    const values = [_]?u8{ 7, 5, 20, 4, 6, 15, 33, 2, null, null, null, 10, null, 25, null };
-    const expected_ordering = [_]?u8{ 2, 4, 5, 6, 7, 10, 15, 20, 25, 33 };
+    const values = [_]u8{ 7, 5, 20, 4, 6, 15, 33, 2, 10, 25 };
+    const expected_ordering = [_]u8{ 2, 4, 5, 6, 7, 10, 15, 20, 25, 33 };
 
     // Insert values into BST
     for (values) |value| {
@@ -161,6 +224,32 @@ test "inorder traversal over BST with null nodes produces correct ordering of vi
     var count: usize = 0;
     while (iterator.next()) |item| {
         try std.testing.expectEqual(expected_ordering[count], item);
+        count += 1;
+    }
+
+    // Free iterator and BST
+    iterator.free();
+    bst.free();
+}
+
+test "inserting elements into binary search tree produces correct ordering" {
+    const allocator = std.testing.allocator;
+    var bst = try BinarySearchTree.new(allocator);
+    const values = [_]u8{ 7, 1, 3, 9, 14, 4, 19, 18, 10, 2, 31, 16, 5, 29, 11 };
+    const inorder_ordering = [_]u8{ 1, 2, 3, 4, 5, 7, 9, 10, 11, 14, 16, 18, 19, 29, 31 };
+
+    // Insert values into BST
+    for (values) |value| {
+        try bst.insert(value);
+    }
+
+    // Get eagerly evaluated iterator over BST for performing inorder traversal
+    var iterator = try bst.inorderTraversal();
+
+    // Traverse inorder iterator and check the values are in the expected ordering
+    var count: usize = 0;
+    while (iterator.next()) |item| {
+        try std.testing.expectEqual(inorder_ordering[count], item);
         count += 1;
     }
 
